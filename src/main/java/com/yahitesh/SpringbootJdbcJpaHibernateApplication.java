c6f5c19eb4bad66d2aaad33619e37f0dada0594e
Hitesh Yadav <hiteshyadav1992@gmail.com>
2021-06-18 14:27:38 +0530

    hibernate-jpa-tutorial : setup spring boot project with Jdbc, jpa, H2, web(web dependency for H2 in memory DB console ) dependency

---
+++ src/main/java/com/yahitesh/SpringbootJdbcJpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.yahitesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJdbcJpaHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJdbcJpaHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yahitesh/SpringbootJdbcJpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yahitesh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJdbcJpaHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
